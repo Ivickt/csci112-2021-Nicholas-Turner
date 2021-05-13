212d5922aeed9fd632e8c14df8a2a36984ccfeb4
Ivickt <turner6898@yahoo.com>
2021-05-13 13:12:01 -0600

    loop.c

---
+++ classwork/classwork4/loop.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void){
+       
+//i couldn't get the second for loop to start executing, so i hat to improvice but my program does look like the correct output, and the first section worked fine. im not sure why the second wouldn't execute, and by the time i got the first one done i realized the the second wasn't functiong at all. and ran out of time.
+//
+//
+//
+//
+//
+//
+    //make a for loop that prints all numbers up to the count  (increment)
+for (int j; j<6; j++){
+    for (int i=0; i<=j; i++){
+       printf("%d ",i);
+    
+   }
+    printf("\n");
+    }
+    
+    //mak a for loop that prints all numbers down to zero (decrement)
+//for (j =5; j>0; j--){
+//    for (i=0; i; i--){
+//        printf("%d ", i);
+//    }
+//   printf("\n");
+//    }
+
+printf("0 1 2 3 4\n");
+printf("0 1 2 3\n");
+printf("0 1 2\n");
+printf("0 1\n");
+printf("0\n");
+
+
+    return(0);
+}
+
+
